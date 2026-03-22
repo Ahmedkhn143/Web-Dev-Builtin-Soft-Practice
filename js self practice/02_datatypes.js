73dc4c310dc406c79817ca14adf588af3f56d807
Ahmedkhn143 <ahmadkha8143@gmail.com>
2026-03-22 09:19:43 +0500

    learn basic js and datatypes of js

---
+++ js self practice/02_datatypes.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+console.log("Ahmad");
+// string data type
+let name = "ahmad "
+// Number Data type
+let age = 21
+// Boolean data type
+let isloggedIn = false
+
+// use bignit if use large numbers
+// always use "" for string datatype we can also use '' for string but this bad practice
+// boolean => true/false
+// null => standalone value (value assigned this is null empty) null is also a object 
+// undefined => vlause is not assigned
+// symbol => is used to define uniqe its type is undefined
+
+
+// Object
+
+console.log(typeof undefined); 
+console.log(typeof null); 
