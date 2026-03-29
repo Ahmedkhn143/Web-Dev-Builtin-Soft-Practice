f45271d05616bc65b6bdf36c115a1d6e52f63633
Ahmedkhn143 <ahmadkha8143@gmail.com>
2026-03-29 11:52:42 +0500

    Learn num and math in js

---
+++ js self practice/06_num_math.js
@@ -0,0 +1,14 @@
+const score = 499
+
+console.log(score)
+const balance = new Number(100)
+
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+
+const otherNumber = 34.8999
+
+console.log(otherNumber);   
+
