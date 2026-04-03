d34f5ab0c2b7fea2a809f68ff75149ec03c6ccdb
Ahmedkhn143 <ahmadkha8143@gmail.com>
2026-04-03 22:33:21 +0500

    array and loop

--- js self practice/script.js
+++ js self practice/script.js
@@ -1,2 +1,12 @@
 console.log("hello World");
  
+// js array and for loop practice
+let expenses = [
+  { title: "Food", amount: 200 },
+  { title: "Travel", amount: 150 },
+  { title: "Rent", amount: 500 }
+];
+
+for (let i = 0; i < expenses.length; i++) {
+  console.log(expenses[i].title);
+}
