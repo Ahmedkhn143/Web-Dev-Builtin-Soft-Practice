4e62ec82175b9c9009c1a8768c4ce23988f8953e
Ahmedkhn143 <ahmadkha8143@gmail.com>
2026-04-05 23:19:35 +0500

    learn array basic in js

---
+++ js self practice/02_basic/01_array.js
@@ -0,0 +1,23 @@
+// array
+
+const array = [0,1,2,3,4,5,6,7];
+
+const Heros =["Quid-e-Azam","Shamsheer-e-Ali","Shamsheer-e-Hind","Shamsheer-e-Pakistan"];
+// console.log(array); // print the entire array
+// console.log(array[3]); // access the element at index 3
+// console.log(Heros[2]); // access the element at index 2
+
+// array method
+
+array.push(8); // add an element to the end of the array
+array.push(9);
+array.push(10);
+
+// console.log(array); // print the modified array
+
+array.pop(); // remove the last element from the array
+
+array.unshift(8); // add  an element to the beginning of the array
+array.shift(); // remove the first element from the array 
+
+console.log(array); // print the modified array
