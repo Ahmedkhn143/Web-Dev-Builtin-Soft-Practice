2b94e12a296da64adbe022f1359581ed48dcdecd
Ahmedkhn143 <ahmadkha8143@gmail.com>
2026-04-06 11:55:55 -0700

    learn swith in js builtin

---
+++ JavaScript/script.js
@@ -0,0 +1,14 @@
+// Switch in js
+let day = "Monday"
+day === "Monday"
+
+switch (day) {
+    case "Monday":
+        console.log("First day of the week")
+        break;
+    case "Friday":
+        console.log("First day of the week")
+        break;
+    default:
+        console.log("Not a valid day")
+}
