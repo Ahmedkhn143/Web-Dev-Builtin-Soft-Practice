6831cb07a71c5a5d502906a2f5ea07565b74c724
Ahmedkhn143 <ahmadkha8143@gmail.com>
2026-04-07 18:48:04 +0500

    Add assignment and JavaScript practice files

---
+++ Assignment 2/assignment.js
@@ -0,0 +1,65 @@
+// Question 1
+// check number is positive, negative or zero
+let num = prompt("Enter a number:");
+if (num > 0){
+    // console.log("Number is positive" )
+}
+else if (num <0){
+    // console.log("Number is negative")
+}
+else{
+    // console.log("Number is zero")
+}
+
+// Question 2
+// check age group
+let age = prompt("Enter your age:");
+if (age < 13){
+    // console.log("You are a child ")
+}
+else if (age <20){
+    // console.log("You are a teenager")
+}
+else {
+    // console.log("You are an adult")
+}
+
+// Question 3
+// check number is even or odd
+let number = prompt("Enter a number:");
+if (number % 2 === 0){
+    // console.log("Number is even")
+}
+else{
+    // console.log("Number is odd")
+}
+
+// Question 4
+// check grade based on marks
+let marks = prompt("Enter your marks:");
+
+if (marks >= 90) {
+  // console.log(" A");
+} else if (marks >= 75) {
+  // console.log(" B");
+} else if (marks >= 50) {
+  // console.log(" C");
+} else {
+  // console.log(" Fail");
+}
+
+// Question 5
+// check greatest of two numbers
+let first = prompt("Enter first number:");
+let second = prompt("Enter second number:");
+
+if (first > second) {
+  // console.log("First number is greater");
+} 
+else if (second > first)
+     {
+  // console.log("Second number is greater");
+}
+ else {
+  // console.log("Both are equal");
+}
