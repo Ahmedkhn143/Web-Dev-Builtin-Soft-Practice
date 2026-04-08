bfa83531d95318381dff8bd42fa5bc18e8e207b2
Ahmedkhn143 <ahmadkha8143@gmail.com>
2026-04-08 22:16:17 +0500

    learn function

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,14 +1,65 @@
-// Switch in js
-let day = "Monday"
-day === "Monday"
+// // Switch in js
+// // let day = "Monday"
+// // day === "Monday"
 
-switch (day) {
-    case "Monday":
-        console.log("First day of the week")
-        break;
-    case "Friday":
-        console.log("First day of the week")
-        break;
-    default:
-        console.log("Not a valid day")
-}
+// // switch (day) {
+// //     case "Monday":
+// //         console.log("First day of the week")
+// //         break;
+// //     case "Friday":
+// //         console.log("First day of the week")
+// //         break;
+// //     default:
+// //         console.log("Not a valid day")
+// // }
+
+// // deep copy and shallow copy in js
+
+// // for(let xyz in person){
+// // console.log('Key is: ', xyz, 'Value is: ', person[xyz])
+// // }
+
+// // Function in js
+
+// // function xyx("parameter"){
+// // return
+// // }
+
+// // function pintName(){
+// //     console.log("My name is ahmad")
+
+// // }
+// // pintName()
+
+// function pintName(name){
+//     console.log('My name is ' + name)
+// }
+// pintName("Ahmad")
+// printName("Ali")
+// printName("Hassan")
+
+
+// function printName(dob){
+//     let date = new Date()
+//     let age = date.getFullYear() - dob
+//     console.log('Your age is ' + age)
+// }
+// printName(2000)
+// function printName(name='Ameer'){
+//     // let date = new Date()
+//     // let age = date.getFullYear() -dob
+//     console.log(`Hello my name is ${name}`)
+// }
+
+
+
+
+// // printName(2000)
+// printName()
+// printName('Anum')
+// printName('Arslan')
+
+let num1 = prompt("Enter first number:");
+let num2 = prompt("Enter second number ");
+let num3 = num1 + num2 ;
+console.log(num3)
---
+++ JavaScript/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+for(let xyz in person){
+// console.log('Key is: ', xyz, 'Value is: ', person[xyz])
+// }
